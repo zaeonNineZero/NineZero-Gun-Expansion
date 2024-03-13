f117c302c730205882913a08006ec161e4db2e1f
zaeonNineZero <zaeonnine0@gmail.com>
2024-03-13 14:20:18 -0400

    Tweaks to the Ballistic Scope's model and ADS parameters to address feedback raised in #16

--- src/main/java/zaeonninezero/nzgmaddon/common/Scopes.java
+++ src/main/java/zaeonninezero/nzgmaddon/common/Scopes.java
@@ -10,5 +10,5 @@ public class Scopes
 {
     public static final Scope DOT_SIGHT = Scope.builder().aimFovModifier(0.8F).modifiers(ExtraGunModifiers.QUICK_ADS).build();
     public static final Scope CHEVRON_SCOPE = Scope.builder().aimFovModifier(0.45F).modifiers(GunModifiers.SLOW_ADS).build();
-    public static final Scope BALLISTIC_SCOPE = Scope.builder().aimFovModifier(0.18F).modifiers(GunModifiers.SLOWER_ADS).build();
+    public static final Scope BALLISTIC_SCOPE = Scope.builder().aimFovModifier(0.20F).modifiers(GunModifiers.SLOWER_ADS).build();
 }
