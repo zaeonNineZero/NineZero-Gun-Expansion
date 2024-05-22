fb64c7a4cea03117d51cfd440323125e1756d524
zaeonNineZero <zaeonnine0@gmail.com>
2024-05-22 15:32:23 -0400

    Update build.gradle and mods.toml. Creative tab now has all NZGE items.

--- src/main/java/zaeonninezero/nzgmaddon/init/initItems.java
+++ src/main/java/zaeonninezero/nzgmaddon/init/initItems.java
@@ -35,50 +35,50 @@ public class initItems {
      */
 	 
 	//Guns
-    public static final RegistryObject<GunItem> REVOLVER = ITEMS.register("revolver", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<GunItem> HEAVY_PISTOL = ITEMS.register("heavy_pistol", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<GunItem> HEAVY_REVOLVER = ITEMS.register("heavy_revolver", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<GunItem> UZI = ITEMS.register("uzi", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<GunItem> SUBMACHINE_GUN = ITEMS.register("submachine_gun", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-	public static final RegistryObject<GunItem> COMPACT_SMG = ITEMS.register("compact_smg", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<GunItem> RAPID_SMG = ITEMS.register("rapid_smg", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<GunItem> PUMP_SHOTGUN = ITEMS.register("pump_shotgun", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<GunItem> HUNTING_SHOTGUN = ITEMS.register("hunting_shotgun", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<GunItem> DOUBLE_BARRELED_SHOTGUN = ITEMS.register("double_barreled_shotgun", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-	public static final RegistryObject<GunItem> AUTOMATIC_SHOTGUN = ITEMS.register("automatic_shotgun", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<GunItem> HEAVY_ASSAULT_RIFLE = ITEMS.register("heavy_assault_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<GunItem> BATTLE_RIFLE = ITEMS.register("battle_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<GunItem> BULLPUP_RIFLE = ITEMS.register("bullpup_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-	public static final RegistryObject<GunItem> MACHINE_GUN = ITEMS.register("machine_gun", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-	public static final RegistryObject<GunItem> INFANTRY_RIFLE = ITEMS.register("infantry_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-	public static final RegistryObject<GunItem> AUTOMATIC_SNIPER_RIFLE = ITEMS.register("automatic_sniper_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<GunItem> HUNTING_RIFLE = ITEMS.register("hunting_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<GunItem> BOLT_ACTION_RIFLE = ITEMS.register("bolt_action_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<GunItem> SNIPER_RIFLE = ITEMS.register("sniper_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
+    public static final RegistryObject<GunItem> REVOLVER = ITEMS.register("revolver", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<GunItem> HEAVY_PISTOL = ITEMS.register("heavy_pistol", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<GunItem> HEAVY_REVOLVER = ITEMS.register("heavy_revolver", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<GunItem> UZI = ITEMS.register("uzi", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<GunItem> SUBMACHINE_GUN = ITEMS.register("submachine_gun", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+	public static final RegistryObject<GunItem> COMPACT_SMG = ITEMS.register("compact_smg", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<GunItem> RAPID_SMG = ITEMS.register("rapid_smg", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<GunItem> PUMP_SHOTGUN = ITEMS.register("pump_shotgun", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<GunItem> HUNTING_SHOTGUN = ITEMS.register("hunting_shotgun", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<GunItem> DOUBLE_BARRELED_SHOTGUN = ITEMS.register("double_barreled_shotgun", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+	public static final RegistryObject<GunItem> AUTOMATIC_SHOTGUN = ITEMS.register("automatic_shotgun", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<GunItem> HEAVY_ASSAULT_RIFLE = ITEMS.register("heavy_assault_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<GunItem> BATTLE_RIFLE = ITEMS.register("battle_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<GunItem> BULLPUP_RIFLE = ITEMS.register("bullpup_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+	public static final RegistryObject<GunItem> MACHINE_GUN = ITEMS.register("machine_gun", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+	public static final RegistryObject<GunItem> INFANTRY_RIFLE = ITEMS.register("infantry_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+	public static final RegistryObject<GunItem> AUTOMATIC_SNIPER_RIFLE = ITEMS.register("automatic_sniper_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<GunItem> HUNTING_RIFLE = ITEMS.register("hunting_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<GunItem> BOLT_ACTION_RIFLE = ITEMS.register("bolt_action_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<GunItem> SNIPER_RIFLE = ITEMS.register("sniper_rifle", () -> new GunItem(new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
 	
 	//Ammo
-	public static final RegistryObject<Item> MEDIUM_BULLET = ITEMS.register("medium_bullet", () -> new AmmoItem(new Item.Properties().tab(GunMod.GROUP)));
+	public static final RegistryObject<Item> MEDIUM_BULLET = ITEMS.register("medium_bullet", () -> new AmmoItem(new Item.Properties().tab(nzgmAddon.GROUP)));
 	
 	//Scopes / Sights
-	public static final RegistryObject<Item> DOT_SIGHT = ITEMS.register("dot_sight", () -> new ScopeItem(Scopes.DOT_SIGHT, new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-	public static final RegistryObject<Item> CHEVRON_SCOPE = ITEMS.register("chevron_scope", () -> new ScopeItem(Scopes.CHEVRON_SCOPE, new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-	public static final RegistryObject<Item> BALLISTIC_SCOPE = ITEMS.register("ballistic_scope", () -> new ScopeItem(Scopes.BALLISTIC_SCOPE, new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
+	public static final RegistryObject<Item> DOT_SIGHT = ITEMS.register("dot_sight", () -> new ScopeItem(Scopes.DOT_SIGHT, new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+	public static final RegistryObject<Item> CHEVRON_SCOPE = ITEMS.register("chevron_scope", () -> new ScopeItem(Scopes.CHEVRON_SCOPE, new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+	public static final RegistryObject<Item> BALLISTIC_SCOPE = ITEMS.register("ballistic_scope", () -> new ScopeItem(Scopes.BALLISTIC_SCOPE, new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
 	
 	//Barrels
-    public static final RegistryObject<Item> FLASH_HIDER  = ITEMS.register("flash_hider", () -> new BarrelItem(Barrel.create(4,ExtraGunModifiers.FLASH_HIDER_EFFECT), new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<Item> MUZZLE_BRAKE  = ITEMS.register("muzzle_brake", () -> new BarrelItem(Barrel.create(5.3f,ExtraGunModifiers.MUZZLE_BRAKE_EFFECT), new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<Item> EXTENDED_BARREL  = ITEMS.register("extended_barrel", () -> new BarrelItem(Barrel.create(6,ExtraGunModifiers.EXTENDED_BARREL_EFFECT), new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<Item> IMPROVED_SILENCER = ITEMS.register("improved_silencer", () -> new BarrelItem(Barrel.create(12.2F, GunModifiers.SILENCED, ExtraGunModifiers.MUFFLED_IMPROVED), new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<Item> PERFORMANCE_SILENCER = ITEMS.register("performance_silencer", () -> new BarrelItem(Barrel.create(12.2F, GunModifiers.SILENCED, ExtraGunModifiers.MUFFLED_PERFORMANCE), new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
+    public static final RegistryObject<Item> FLASH_HIDER  = ITEMS.register("flash_hider", () -> new BarrelItem(Barrel.create(4,ExtraGunModifiers.FLASH_HIDER_EFFECT), new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<Item> MUZZLE_BRAKE  = ITEMS.register("muzzle_brake", () -> new BarrelItem(Barrel.create(5.3f,ExtraGunModifiers.MUZZLE_BRAKE_EFFECT), new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<Item> EXTENDED_BARREL  = ITEMS.register("extended_barrel", () -> new BarrelItem(Barrel.create(6,ExtraGunModifiers.EXTENDED_BARREL_EFFECT), new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<Item> IMPROVED_SILENCER = ITEMS.register("improved_silencer", () -> new BarrelItem(Barrel.create(12.2F, GunModifiers.SILENCED, ExtraGunModifiers.MUFFLED_IMPROVED), new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<Item> PERFORMANCE_SILENCER = ITEMS.register("performance_silencer", () -> new BarrelItem(Barrel.create(12.2F, GunModifiers.SILENCED, ExtraGunModifiers.MUFFLED_PERFORMANCE), new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
 	
 	//Stocks
-    public static final RegistryObject<Item> SHORT_STOCK  = ITEMS.register("short_stock", () -> new StockItem(Stock.create(ExtraGunModifiers.SLIGHT_BETTER_CONTROL), new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<Item> DYEABLE_TACTICAL_STOCK  = ITEMS.register("dyeable_tactical_stock", () -> new StockItem(Stock.create(GunModifiers.STABILISED), new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<Item> SOLID_STOCK = ITEMS.register("solid_stock", () -> new StockItem(Stock.create(ExtraGunModifiers.SOLIDLY_STABILISED), new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<Item> STABILIZING_STOCK = ITEMS.register("stabilizing_stock", () -> new StockItem(Stock.create(ExtraGunModifiers.EXTRA_STABILISED), new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<Item> CARBINE_STOCK  = ITEMS.register("carbine_stock", () -> new StockItem(Stock.create(ExtraGunModifiers.CARBINE_STABILISED), new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
-    public static final RegistryObject<Item> MARKSMAN_STOCK  = ITEMS.register("marksman_stock", () -> new StockItem(Stock.create(ExtraGunModifiers.MARKSMAN_STABILISED), new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
+    public static final RegistryObject<Item> SHORT_STOCK  = ITEMS.register("short_stock", () -> new StockItem(Stock.create(ExtraGunModifiers.SLIGHT_BETTER_CONTROL), new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<Item> DYEABLE_TACTICAL_STOCK  = ITEMS.register("dyeable_tactical_stock", () -> new StockItem(Stock.create(GunModifiers.STABILISED), new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<Item> SOLID_STOCK = ITEMS.register("solid_stock", () -> new StockItem(Stock.create(ExtraGunModifiers.SOLIDLY_STABILISED), new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<Item> STABILIZING_STOCK = ITEMS.register("stabilizing_stock", () -> new StockItem(Stock.create(ExtraGunModifiers.EXTRA_STABILISED), new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<Item> CARBINE_STOCK  = ITEMS.register("carbine_stock", () -> new StockItem(Stock.create(ExtraGunModifiers.CARBINE_STABILISED), new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
+    public static final RegistryObject<Item> MARKSMAN_STOCK  = ITEMS.register("marksman_stock", () -> new StockItem(Stock.create(ExtraGunModifiers.MARKSMAN_STABILISED), new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
 	
 	//Grips / Under Barrel
-    public static final RegistryObject<Item> HORIZONTAL_GRIP  = ITEMS.register("horizontal_grip", () -> new UnderBarrelItem(UnderBarrel.create(ExtraGunModifiers.HORIZONTAL_CONTROL), new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
+    public static final RegistryObject<Item> HORIZONTAL_GRIP  = ITEMS.register("horizontal_grip", () -> new UnderBarrelItem(UnderBarrel.create(ExtraGunModifiers.HORIZONTAL_CONTROL), new Item.Properties().stacksTo(1).tab(nzgmAddon.GROUP)));
 }
